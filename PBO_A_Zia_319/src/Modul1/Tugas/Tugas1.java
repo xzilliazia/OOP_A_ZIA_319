50b5a4693bdf82abbc2efe1153e7bde65656b40c
xzilliazia <mziaulhaq05@gmail.com>
2025-02-24 19:34:14 +0700

    third

--- PBO_A_Zia_319/src/Modul1/Tugas/Tugas1.java
+++ PBO_A_Zia_319/src/Modul1/Tugas/Tugas1.java
@@ -1,30 +1,45 @@
 package Modul1.Tugas;
 
+import java.awt.GridBagLayout;
 import java.util.Scanner;
 
 public class Tugas1 {
-    try (Scanner input = new Scanner(System.in)){
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {   
             System.out.print("Masukkan Pilihan: ");
-            choise = input.nextLine();
+            int choise = input.nextLine();
+            input.nextLine();
 
-            String.inputUsernameAdmin = "Admin";
-            String.inputPasswordAdmin = "admin12";
-            String.inputUsernameSiswa = "Ziaulhaq";
-            String.inputNIMSiswa = "319";
-
+            //Data Login
+            String inputUsernameAdmin = "Admin";
+            String inputPasswordAdmin = "admin12";
+            String inputUsernameSiswa = "Ziaulhaq";
+            String inputNIMSiswa = "319";
 
         switch (choise) {
             case 1 :
                 System.out.print("Masukkan Username: ");
-                username = input.nextLine();
-    
-                if (input.equals(inputUsernameAdmin) && (input.equals(inputPasswordAdmin))){
+                String username = input.nextLine();
+                System.out.print("Masukkan Password: ");
+                String password = input.nextLine();
+
+                if (username.equals(inputUsernameAdmin) && (password.equals(inputPasswordAdmin))) {
                     System.out.println("Login anda berhasil!");
+                } else {
+                    System.out.println("Gagal Login.");
                 }
                 break;
             case 2 :
-                System.out.print("Masukkan Password: ");
-                userpassword = input.nextLine();
+                System.out.print("Masukkan Nama: ");
+                String usermh = input.nextLine();
+                System.out.print("Masukkan NIM: ");
+                String nimpassword = input.nextLine();
+
+                if (usermh.equals(inputUsernameSiswa) && (nimpassword.equals(inputNIMSiswa))){
+                    System.out.println("Login berhasil!");
+                } else {
+                    System.out.println("Gagal Login.");
+                }
                 break;
             default:
                 throw new AssertionError();
@@ -36,4 +51,5 @@ public class Tugas1 {
             }
             
         }
+    }
 }
