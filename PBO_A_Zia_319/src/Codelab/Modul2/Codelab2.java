4c9a7acd5e22fabb54e3e26ef6bf8664b4dd83b3
xzilliazia <mziaulhaq05@gmail.com>
2025-03-10 22:21:31 +0700

    pre

--- PBO_A_Zia_319/src/Codelab/Modul2/Codelab2.java
+++ PBO_A_Zia_319/src/Codelab/Modul2/Codelab2.java
@@ -1,5 +1,27 @@
 package Codelab.Modul2;
 
+class BankAccount{
+    private String accountNumber;
+    private String ownerName;
+    private Double balance;
+
+    BankAccount(String accountNumber, String ownerName, Double balance){
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    void displayInfo(){
+        
+    }
+
+    String depositMoney(Double amount){
+        
+    }
+}
+
 public class Codelab2 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
