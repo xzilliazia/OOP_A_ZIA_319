466c2b38a085872320f6a7523d9a0324d8fc24dc
xzilliazia <mziaulhaq05@gmail.com>
2025-03-11 11:44:41 +0700

    name-change

---
+++ PBO_A_Zia_319/src/Codelab/Modul2/BankMain.java
@@ -0,0 +1,17 @@
+package Codelab.Modul2;
+
+public class BankMain {
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount("319","Zia", 2000d);
+        BankAccount account2 = new BankAccount("024","ia", 4000d);
+        //akun 1
+        account1.displayInfo();
+        account2.displayInfo();
+        account1.depositMoney(70000d);
+        account1.withDrawMoney(78000d);
+
+        //akun 2
+        account2.depositMoney(80000d);
+        account2.withDrawMoney(85000d);
+    }
+}
