b9db9d4b07ebb34e529ce83d1723ccf94ba10296
xzilliazia <mziaulhaq05@gmail.com>
2025-03-16 00:58:23 +0700

    minigame-class

---
+++ PBO_A_Zia_319/src/Task/Modul3/GameChar.java
@@ -0,0 +1,29 @@
+package Task.Modul3;
+
+class GameChar {
+    private String name;
+    private int Health;
+
+    GameChar (String name, int Health ){
+        this.name = name;
+        this.Health = Health;
+    }
+    //getter
+    public String GetName(){
+        return this.name;
+    }
+    public int GetHealth(){
+        return this.Health;
+    }
+    //setter
+    public void SetName(String name){
+        this.name = name;
+    }
+    public void SetHealth(int Health){
+        this.Health = Health;
+    }
+
+    public void Attack(GameChar Target){
+        System.out.println(name + " Menyerang "+ Target.GetName());
+    }
+}
