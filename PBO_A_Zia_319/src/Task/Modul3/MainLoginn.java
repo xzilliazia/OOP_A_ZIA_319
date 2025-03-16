68574f686e4e490eb4c4db6bb21b1eec15ceb5a0
xzilliazia <mziaulhaq05@gmail.com>
2025-03-16 12:43:42 +0700

    super&main-class

---
+++ PBO_A_Zia_319/src/Task/Modul3/MainLoginn.java
@@ -0,0 +1,40 @@
+package Task.Modul3;
+import java.util.Scanner;
+
+public class MainLoginn {
+    public static void main(String[] args) {
+        //object dai 2 class
+        AdminClass admin1 = new AdminClass("Zia", "319");
+        StudentClass student1 = new StudentClass("dian", "24");
+
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.println("Pilih jenis Login:\n1. Admin\n2. Student\n3. Keluar");
+            System.out.print("Masukkan pilihan (1/2/3): ");
+            String choice = input.nextLine().trim();
+
+            if (choice.equals("3")){
+                System.out.println("Progam Keluar.");
+                break;
+            } else if (choice.equals("1")){
+                if (loginUser(input, admin1)) break;
+            } else if (choice.equals("2")){
+                if (loginUser(input, student1)) break;
+            } else {
+                System.out.println("Input Invalid. Harap Masukkan 1/2/3.");
+            }
+        } input.close();
+    }
+    
+    private static boolean loginUser (Scanner input, UserLoginClass user){
+        System.out.print("\nMasukkan Username: ");
+        String userName = input.nextLine();
+        System.out.print("Masukkan Password: ");
+        String userPassw = input.nextLine();
+
+        boolean isLogin = user.Login(userName, userPassw);
+        user.displayInfo(isLogin);
+        return isLogin;
+
+    }
+}
---
+++ PBO_A_Zia_319/src/Task/Modul3/UserLoginClass.java
@@ -0,0 +1,28 @@
+package Task.Modul3;
+
+public class UserLoginClass {
+    private String userName;
+    private String userPassw;
+
+    public UserLoginClass(String userName, String userPassw){
+        this.userName = userName;
+        this.userPassw = userPassw;
+    }
+    public String getName(){
+        return userName;
+    }
+    public String getPassw(){
+        return userPassw;
+    }
+    public void setName(String userName){
+        this.userName = userName;
+    }
+    public void setStudentId(String userPassw){
+        this.userPassw = userPassw;
+    }
+
+    public boolean Login(String inputUserName, String inputUserPassw){
+        return false;
+    }
+    public void displayInfo(boolean isLogin){}
+}
