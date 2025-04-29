69550ad9596568a9957b1cfc44f64f8d864e6e63
xzilliazia <mziaulhaq05@gmail.com>
2025-04-29 23:35:01 +0700

    loginsystem-

---
+++ PBO_A_Zia_319/src/Task/Modul4/com/praktikum/main/loginSystem.java
@@ -0,0 +1,32 @@
+package Task.Modul4.com.praktikum.main;
+import Task.Modul4.com.praktikum.actions.*;
+import Task.Modul4.com.praktikum.users.*;
+import java.util.Scanner;
+
+public class loginSystem {
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa("Ziaulhaq", "319");
+        Admin admin1 = new Admin("Bakri", "bakri01");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Pilih Jenis Login (1. Admin/2. Mahsiswa): ");
+        int inputLoginPage = input.nextInt();
+        input.nextLine();
+        
+        switch (inputLoginPage) {
+            case 1:
+                admin1.login();
+                admin1.displayAppMenu();
+                break;
+            case 2:
+                mhs1.login();
+                mhs1.displayAppMenu();
+                break;
+            default:
+                break;
+        }
+
+        
+    
+    }
+}
