9322c2b6c35695648f90ff969906190de271b2c1
xzilliazia <mziaulhaq05@gmail.com>
2025-04-29 23:35:18 +0700

    admin-type

--- PBO_A_Zia_319/src/Task/Modul4/com/praktikum/users/Admin.java
+++ PBO_A_Zia_319/src/Task/Modul4/com/praktikum/users/Admin.java
@@ -1,4 +1,6 @@
 package Task.Modul4.com.praktikum.users;
+import java.util.Scanner;
+
 import Task.Modul4.com.praktikum.actions.*;
 
 public class Admin extends userClass implements AdminActions{
@@ -9,8 +11,22 @@ public class Admin extends userClass implements AdminActions{
 
     @Override
     public void login(){
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan Username: ");
+        String inputUsername = input.nextLine();
+
+        System.out.print("Masukkan Password: ");
+        String inputPassword = input.nextLine();
+
+        if (inputUsername.equals(this.username) && inputPassword.equals(this.password)){
+            System.out.println("Login BErhasil!");
+            
+        } else {
+            System.out.println("Login Gagal!");
+        }
     }
+
     @Override
     public void manageItem(){
         System.out.println("Fitur kelola barang belum tersedia");
@@ -19,5 +35,21 @@ public class Admin extends userClass implements AdminActions{
     @Override
     public void manageUser(){
         System.out.println("Fitur kelola mahasiswa belum tersedia");
+    }
+
+    @Override
+    public void displayAppMenu(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Kelola Laporan Barang\n2. Kelola daftar Mahasiswa\n0. Logout");
+        String choice = input.nextLine().trim();
+        if (choice.equals("1")){
+            manageItem();
+        } else if (choice.equals("2")){
+            manageUser();
+        } else if (choice.equals("0")) {
+            System.out.println("Progam Keluar");
+        } else {
+            System.out.println("Pilhan tidak Valid");
+        }
     }
 }
