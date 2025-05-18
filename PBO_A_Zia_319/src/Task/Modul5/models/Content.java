55e7bcbda4b3c7f9bf65be62b732c720c6559e1f
xzilliazia <mziaulhaq05@gmail.com>
2025-05-18 22:05:05 +0700

    noun

---
+++ PBO_A_Zia_319/src/Task/Modul5/models/Content.java
@@ -0,0 +1,22 @@
+package Task.Modul5.models;
+
+public abstract class Content {
+    private String itemName;
+    private String deskriptionItem;
+    private String itemlocation;
+    private String itemStatus;
+    
+     public Content(String itemName, String deskriptionItem, String location, String status){
+         this.itemName = itemName;
+         this.deskriptionItem = deskriptionItem;
+         this.itemlocation = location;
+         this.itemStatus = status;
+     }
+
+    public String getItemName() { return itemName; }
+    public String getDeskriptionItem() { return deskriptionItem; }
+    public String getItemlocation(){ return itemlocation; }
+    public String getItemStatus(){ return itemStatus; }
+
+    public abstract void info();
+}
---
+++ PBO_A_Zia_319/src/Task/Modul5/models/Entity.java
@@ -0,0 +1,16 @@
+package Task.Modul5.models;
+
+public abstract class Entity {
+    private int id;
+    private String name;
+
+    public Entity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getName() { return name; }
+
+    public int getId() {
+        return id;
+    }
+}
